f3e2c03ca21b5c2d0220d98a6fa190044cbd4c2e
prakashkrish2022 <prakashmech925@gmail.com>
2022-04-23 09:29:12 -0700

    new code

---
+++ src/org/tcs/Hello.java
@@ -0,0 +1,16 @@
+package org.tcs;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		 for (int i = 1; i <= 3; i++) {
+		      for (int j = i + 1; j <= i; j++) {
+		        System.out.println(j);
+		      }
+
+		    }
+
+		  }
+
+		}
+
---
+++ src/org/tcs/Maths.java
@@ -0,0 +1,15 @@
+package org.tcs;
+
+public class Maths {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 3; i++) {
+		      for (int j = i + 1; j <= 3;j++) {
+		        System.out.println(j);
+		      }
+
+		    }
+
+		  }
+
+		}
