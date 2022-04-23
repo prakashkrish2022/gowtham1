a7d8b022f50c35ce5c26bf685b0a5abb462db6a8
prakashkrish2022 <prakashmech925@gmail.com>
2022-04-23 09:37:23 -0700

    new

--- src/loops/Maths.java
+++ src/loops/Maths.java
@@ -7,7 +7,7 @@ public static void main(String[] args) {
 	 for (int i = 1; i <= 3; i++) {
 	      for (int j = i + 1; j <= i; j++) {
 	        System.out.println(j);
-	      
+	         int e ;
 	      }
 	 }
 }
